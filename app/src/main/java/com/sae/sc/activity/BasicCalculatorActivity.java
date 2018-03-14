2fd2cc5d73660570197b3f134c1fbbd8f5fc32ee
LiuXiapu <32235286+LiuXiapu@users.noreply.github.com>
2018-03-14 23:45:56 +0900

    add MathFormulaView and ResizingEditText & forbin typing in EditText

--- app/src/main/java/com/sae/sc/activity/BasicCalculatorActivity.java
+++ app/src/main/java/com/sae/sc/activity/BasicCalculatorActivity.java
@@ -3,12 +3,15 @@ package com.sae.sc.activity;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.text.InputType;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.sae.sc.R;
 import com.sae.sc.activity.base.AbstractCalculatorActivity;
 import com.sae.sc.fragment.KeyboardFragment;
 import com.sae.sc.listener.KeyboardListener;
+import com.sae.sc.view.ResizingEditText;
 
 public class BasicCalculatorActivity extends AbstractCalculatorActivity
         implements KeyboardListener {
@@ -20,9 +23,23 @@ public class BasicCalculatorActivity extends AbstractCalculatorActivity
         setContentView(R.layout.activity_basic_calculator);
 
         initView();
+        hideSoftKeyboard();
         initKeyboard();
 
     }
+
+    private void hideSoftKeyboard() {
+        final ResizingEditText inputView = (ResizingEditText) findViewById(R.id.txtDisplay);
+        //关闭软键盘和光标
+        inputView.setInputType(InputType.TYPE_NULL);
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(inputView.getWindowToken(), 0);
+
+        //禁止所有输入，只能通过按钮更改edittext
+        inputView.setFocusable(false);
+    }
+
 
     @Override
     public void insertText(String s) {
