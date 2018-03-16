43e585df16bc3c3a5191baca34df96a914c0bc70
LiuXiapu <32235286+LiuXiapu@users.noreply.github.com>
2018-03-16 14:45:52 +0900

    add animation

--- app/src/main/java/com/sae/sc/activity/BasicCalculatorActivity.java
+++ app/src/main/java/com/sae/sc/activity/BasicCalculatorActivity.java
@@ -1,5 +1,8 @@
 package com.sae.sc.activity;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.Nullable;
@@ -10,8 +13,10 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
@@ -20,10 +25,12 @@ import android.widget.Toast;
 import com.sae.sc.R;
 import com.sae.sc.activity.base.AbstractCalculatorActivity;
 import com.sae.sc.fragment.KeyboardFragment;
+import com.sae.sc.listener.AnimationFinishedListener;
 import com.sae.sc.listener.KeyboardListener;
 import com.sae.sc.utils.Evaluator;
 import com.sae.sc.view.CalculatorEditText;
 import com.sae.sc.view.MathFormulaView;
+import com.sae.sc.view.RevealView;
 
 import static com.sae.sc.utils.Evaluator.INPUT_EMPTY;
 
@@ -144,9 +151,24 @@ public class BasicCalculatorActivity extends AbstractCalculatorActivity
 
     @Override
     public void onClear() {
-        mInputDisplay.clear();
-        mMathView.setText(null);
-        setState(CalculatorState.INPUT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = this.getTheme();
+            theme.resolveAttribute(R.attr.colorAccent, typedValue, true);
+            int color = typedValue.data;
+            animateRipple(mDisplayForeground, null, color, new AnimationFinishedListener() {
+                @Override
+                public void onAnimationFinished() {
+                    setState(CalculatorState.INPUT);
+                    mInputDisplay.clear();
+                    mMathView.setText(null);
+                }
+            }, true);
+        } else {
+            mInputDisplay.clear();
+            mMathView.setText(null);
+            setState(CalculatorState.INPUT);
+        }
     }
 
     @Override
@@ -166,13 +188,25 @@ public class BasicCalculatorActivity extends AbstractCalculatorActivity
     }
 
     @Override
-    public void onError(String errorResourceId) {
-        Log.i("main", "onError()");
-
-        setState(CalculatorState.ERROR);
-
-        mMathView.setText(errorResourceId);
-        mMathView.setTextColor(Color.RED);
+    public void onError(final String error) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = this.getTheme();
+            theme.resolveAttribute(R.attr.colorResultError, typedValue, true);
+            int color = typedValue.data;
+            animateRipple(mDisplayForeground, null, color, new AnimationFinishedListener() {
+                @Override
+                public void onAnimationFinished() {
+                    setState(CalculatorState.ERROR);
+                    mMathView.setTextColor(Color.RED);
+                    mMathView.setText(error);
+                }
+            }, true);
+        } else {
+            setState(CalculatorState.ERROR);
+            mMathView.setTextColor(Color.RED);
+            mMathView.setText(error);
+        }
     }
 
     private void setState(CalculatorState state) {
@@ -208,6 +242,64 @@ public class BasicCalculatorActivity extends AbstractCalculatorActivity
 
     public void onResult(final String result) {
         mMathView.setText(result);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void animateRipple(final ViewGroup foreground,
+                               @Nullable View sourceView,
+                               int color, final Animator.AnimatorListener listener, final boolean out) {
+        if (color == -1) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = this.getTheme();
+            theme.resolveAttribute(R.attr.colorAccent, typedValue, true);
+            color = typedValue.data;
+        } else if (color == -2) {
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = this.getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            color = typedValue.data;
+        }
+        //create new reveal view
+        final RevealView revealView = new RevealView(this);
+        revealView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        revealView.setRevealColor(color);
+        //add to viewgroup
+        foreground.addView(revealView);
+
+        final Animator revealAnimator;
+        final int[] clearLocation = new int[2];
+        if (sourceView != null) {
+            sourceView.getLocationInWindow(clearLocation);
+            clearLocation[0] += sourceView.getWidth() / 2;
+            clearLocation[1] += sourceView.getHeight() / 2;
+        } else {
+            clearLocation[0] = foreground.getWidth() / 2;
+            clearLocation[1] = foreground.getHeight() / 2;
+        }
+        final int revealCenterX = clearLocation[0] - revealView.getLeft();
+        final int revealCenterY = clearLocation[1] - revealView.getTop();
+
+        final double x1_2 = Math.pow(revealView.getLeft() - revealCenterX, 2);
+        final double x2_2 = Math.pow(revealView.getRight() - revealCenterX, 2);
+        final double y_2 = Math.pow(revealView.getTop() - revealCenterY, 2);
+        final float revealRadius = (float) Math.max(Math.sqrt(x1_2 + y_2), Math.sqrt(x2_2 + y_2));
+
+        if (out)
+            revealAnimator = ViewAnimationUtils.createCircularReveal(
+                    revealView, revealCenterX, revealCenterY, 0.0f, revealRadius);
+        else
+            revealAnimator = ViewAnimationUtils.createCircularReveal(
+                    revealView, revealCenterX, revealCenterY, revealRadius, 0f);
+
+        revealAnimator.setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+        if (listener != null) revealAnimator.addListener(listener);
+        revealAnimator.addListener(new AnimationFinishedListener() {
+            @Override
+            public void onAnimationFinished() {
+                foreground.removeView(revealView);
+            }
+        });
+        revealAnimator.start();
     }
 
     @Override
